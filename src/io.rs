09353736bb9f9bc953dd75cf824e99b0d431c977
David Terrell <dterrell@gmail.com>
2022-12-10 03:06:04 -0800

    add a utility method for inline test inputs

--- src/io.rs
+++ src/io.rs
@@ -11,3 +11,7 @@ pub fn read_lines(path: &str) -> Result<Vec<String>> {
         .map(|s| s.trim_end().to_string())
         .collect())
 }
+
+pub fn split_str(buf: &str) -> Vec<String> {
+    buf.split('\n').map(|s| s.to_string()).collect()
+}
--- src/prelude.rs
+++ src/prelude.rs
@@ -1,4 +1,4 @@
 pub use crate::day::Day;
 pub use crate::debug::*;
-pub use crate::io::read_lines;
+pub use crate::io::*;
 pub use anyhow::Result;
