f24162e6dc3399642d37cb8d65018d5c941a6d0a
David Terrell <dterrell@gmail.com>
2022-12-17 14:30:37 -0800

    add support for cli arg to just run one day

--- src/runner.rs
+++ src/runner.rs
@@ -1,13 +1,32 @@
 use std::time::Instant;
+use std::env;
 
 use anyhow::Result;
 
 use crate::{day::Day, fetch::Fetcher};
 
+fn day_matcher() -> Box<dyn Fn(u8) -> bool> {
+    let args: Vec<_> = env::args().collect();
+    if args.len() == 1 {
+        return Box::new(|_| true);
+    }
+    let num: Result<u8, _> = args[1].parse();
+    if num.is_err() {
+        eprint!("Invalid argument: {}", args[1]);
+        return Box::new(|_| false);
+    }
+    let val = num.unwrap();
+    return Box::new(move |u| u == val);
+}
+
 pub fn run(year: u32, days: Vec<Box<dyn Day>>) -> Result<()> {
     let fetcher = Fetcher::new(year);
+    let matcher = day_matcher();
     for d in days {
         {
+            if !matcher.as_ref()(d.number()) {
+                continue;
+            }
             fetcher.check(d.number())?;
             print!("Day {} part 1: ", d.number());
             let i1 = Instant::now();
